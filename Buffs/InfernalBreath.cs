0d10b2c55c46d9158e6231dac94ee6b7fdd60bbf
Fazzoliie <vhfazoli@gmail.com>
2021-09-13 17:59:45 -0300

    new sprites

---
+++ Buffs/InfernalBreath.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GearonArsenalMod.Buffs
+{
+    public class InfernalBreath : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = true;
+            DisplayName.SetDefault("Infernal Breath");
+            Description.SetDefault("");
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.thorns += 10;
+        }
+    }
+}
