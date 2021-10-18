418a35ba533bd1e2d555a3a7a5362c14651ea057
Fazoli <86382456+Vitor-Fazoli@users.noreply.github.com>
2021-10-18 17:43:43 -0300

    Design Pattern

---
+++ Common/Players/GreatswordPlayer.cs
@@ -0,0 +1,49 @@
+﻿using GearonArsenalMod.Abstract;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using System.Collections.Generic;
+
+namespace GearonArsenalMod.Common.Players{ 
+
+    public class GreatswordPlayer : ModPlayer{
+
+        public int slayerPower = 0;
+        public int slayerMax = 3;
+
+        public override void PreUpdateBuffs(){
+
+            if((Main.LocalPlayer.HeldItem.ModItem is ItemGreatsword) && !Player.dead){
+
+                if (slayerPower == 1){
+
+                    Player.GetCritChance(DamageClass.Melee) += 5;
+                    Player.meleeSpeed += 0.10f;
+                }
+                else if (slayerPower == 2){
+
+                   Player.GetCritChance(DamageClass.Melee) += 10;
+                   Player.meleeSpeed += 0.15f;
+                }
+                else if (slayerPower == 3){
+
+                    Player.GetCritChance(DamageClass.Melee) += 20;
+                    Player.meleeSpeed += 0.30f;
+                }
+
+                if (slayerPower > slayerMax){
+
+                    slayerPower = slayerMax;
+                }
+            }
+            else{
+
+                slayerPower = 0;
+            }
+        }
+        public override void Kill(double damage, int hitDirection, bool pvp, PlayerDeathReason damageSource){
+
+            Player.ClearBuff(ModContent.BuffType<CursedSkull>());
+        }
+    }
+}
---
+++ Content/Item/Weapons/CopperGreatsword.cs
@@ -0,0 +1,85 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using GearonArsenalMod.Abstract;
+using Microsoft.Xna.Framework;
+
+namespace GearonArsenalMod.Item.Weapons
+{
+	public class CopperGreatsword : ItemGreatsword{
+
+		public override void SetStaticDefaults(){
+
+			DisplayName.SetDefault("Copper Greatsword");
+			Tooltip.SetDefault("");
+		}
+		public override void SetDefaults(){
+
+			//properties - Default
+			Item.damage = ModContent.GetInstance<CopperGreatswordP>().GetDmg();
+			Item.DamageType = DamageClass.Melee;
+			Item.useTime = 50;
+			Item.useAnimation = 50;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.knockBack = ModContent.GetInstance<CopperGreatswordP>().GetKnk();
+			Item.noUseGraphic = true;
+			Item.noMelee = true;
+			Item.shoot = greatsword;
+			Item.channel = true;
+			Item.crit = -4;
+
+			//properties - Item
+			greatsword = ModContent.ProjectileType<CopperGreatswordP>();
+			gStats = ModContent.GetInstance<CopperGreatswordP>();
+			slash = ModContent.ProjectileType<CopperSlash>();
+		}
+        public override void AddRecipes(){
+
+			CreateRecipe()
+			.AddIngredient(ItemID.CopperBar, 9)
+			.AddIngredient(ItemID.CopperShortsword)
+			.AddTile(TileID.Anvils)
+			.Register();
+		}
+	}
+	public class CopperGreatswordP : Greatsword{
+
+		public override void SetDefaults(){
+
+			//properties - Default
+			Projectile.width = 0;
+			Projectile.height = 0;
+			Projectile.aiStyle = 0;
+			Projectile.friendly = true;
+			Projectile.penetrate = -1;
+			Projectile.timeLeft = 124;
+			Projectile.tileCollide = false;
+			Projectile.ignoreWater = true;
+			Projectile.ownerHitCheck = true;
+			Projectile.usesLocalNPCImmunity = true;
+			Projectile.localNPCHitCooldown = -1;
+			Projectile.extraUpdates = 1;
+
+			//properties - Greatsword
+			gDamage = 9;
+			cooldown = 75;
+			aggro = 10;
+			defense = 3;
+			slash = ModContent.ProjectileType<CopperSlash>();
+			wEffect = DustID.Cloud;
+		}
+	}
+	public class CopperSlash : Slash{
+
+		public override void SetStaticDefaults(){
+
+			DisplayName.SetDefault("Cooper Slash");
+			Main.projFrames[Projectile.type] = frames;
+		}
+		public override bool PreDraw(ref Color lightColor){
+
+			lightColor = new(228, 150, 105);
+			return base.PreDraw(ref lightColor);
+		}
+	}
+}
