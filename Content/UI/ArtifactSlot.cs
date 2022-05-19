bd225dc0d07cf95d4d5fab4e7c49253e2b4ad35b
Fazoli <vhfazoli@gmail.com>
2022-05-19 17:06:19 -0300

    testing new acessory slot

---
+++ Content/UI/ArtifactSlot.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace GearonArsenal.Content.UI
+{
+    public class ArtifactSlot : ModAccessorySlot
+    {
+        public override bool IsHidden()
+        {
+            return false;
+        }
+
+        public override bool DrawVanitySlot => false;
+
+        public override bool DrawDyeSlot => false;
+
+        public override string FunctionalTexture => (GetType().Namespace + "." + Name).Replace('.', '/');
+
+        public override bool CanAcceptItem(Item checkItem, AccessorySlotType context)
+        {
+            if (checkItem.rare == ModContent.RarityType<Artifact>())
+                return true;
+
+            return false;
+        }
+
+        public override bool ModifyDefaultSwapSlot(Item item, int accSlotToSwapTo)
+        {
+            if (item.rare == ModContent.RarityType<Artifact>())
+                return true;
+
+            return false;
+        }
+        public override bool IsEnabled()
+        {
+            return true;
+        }
+    }
+}
