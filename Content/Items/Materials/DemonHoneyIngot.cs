09f9fe070a7c884e2b6e144fc61b50b3734d7705
Fazoli <86382456+Vitor-Fazoli@users.noreply.github.com>
2022-08-19 18:41:03 -0300

    more contents (old sprites )

---
+++ Content/Items/Materials/DemonHoneyIngot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+
+namespace VoidArsenal.Content.Items.Materials
+{
+    public class DemonHoneyIngot : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("DemonHoney Ingot");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 30;
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 30;
+            Item.height = 24;
+            Item.material = true;
+            Item.rare = ItemRarityID.Blue;
+            Item.shopCustomPrice = 1000;
+            Item.maxStack = 999;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.DemoniteBar, 2)
+                .AddIngredient(ItemID.BeeWax, 5)
+                .AddTile(TileID.Furnaces)
+                .Register();
+        }
+    }
+}
