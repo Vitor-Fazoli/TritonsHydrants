09516a76ad989aa0c28db68ca41b98c66126a6b7
Fazoli <vhfazoli@gmail.com>
2022-09-08 00:25:10 -0300

    Timer to put artifact again is working

--- Content/UI/ArtifactSlot.cs
+++ Content/UI/ArtifactSlot.cs
@@ -10,16 +10,15 @@ namespace VoidArsenal.Content.UI
 {
     public class ArtifactSlot : ModAccessorySlot
     {
+        int time = 0;
 
         public override void OnMouseHover(AccessorySlotType context)
         {
             Main.hoverItemName = "Artifact";
 
-            bool flag = Main.mouseLeft && Main.mouseLeftRelease;
-
-            if (flag)
+            if (IsEmpty && time != 0)
             {
-                Main.blockMouse = true;
+                Main.hoverItemName = (time / 60).ToString() + " s";
             }
         }
         public override bool IsHidden()
@@ -31,10 +30,32 @@ namespace VoidArsenal.Content.UI
         public override bool DrawVanitySlot => false;
         public override bool DrawDyeSlot => false;
         #endregion
+        public override void PostDraw(AccessorySlotType context, Item item, Vector2 position, bool isHovered)
+        {
+            if(time <= 0)
+            {
+                time = 0;
+            }
 
+            if (IsEmpty && time != 0)
+            {
+                --time;
+            }
+
+            if (!IsEmpty)
+            {
+                time = 1800;
+            }
+        }
+        
+        public override string FunctionalBackgroundTexture => "Terraria/Images/Inventory_Back7";
         public override string FunctionalTexture => (GetType().Namespace + "." + Name).Replace('.', '/');
-        public override bool CanAcceptItem(Item checkItem, AccessorySlotType context) => checkItem.rare == ModContent.RarityType<ArtifactRarity>();
-        public override bool ModifyDefaultSwapSlot(Item item, int accSlotToSwapTo) => item.rare == ModContent.RarityType<ArtifactRarity>();
-        public override bool IsEnabled() => true;
+        public override bool CanAcceptItem(Item checkItem, AccessorySlotType context)
+        {
+            return checkItem.rare == ModContent.RarityType<ArtifactRarity>() && IsEmpty;
+        }
+        public override bool ModifyDefaultSwapSlot(Item item, int accSlotToSwapTo) => item.rare == ModContent.RarityType<ArtifactRarity>() && IsEmpty;
+        public override bool IsEnabled() => time == 0;
+        public override bool IsVisibleWhenNotEnabled() => true;
     }
 }
