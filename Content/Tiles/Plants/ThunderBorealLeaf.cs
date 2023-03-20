98e099669de69836507ebcf85a0d9abf2cb615fa
Fazoli <vhfazoli@gmail.com>
2023-03-20 09:00:31 -0300

    Thunder Boreal Tree - not finished

---
+++ Content/Tiles/Plants/ThunderBorealLeaf.cs
@@ -0,0 +1,11 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DevilsWarehouse.Content.Tiles.Plants
+{
+    public class ThunderBorealLeaf : ModGore
+    {
+        public override string Texture => "ExampleMod/Content/Tiles/Plants/ExampleTree_Leaf";
+        public override void SetStaticDefaults() => GoreID.Sets.SpecialAI[Type] = 3;
+    }
+}
---
+++ Content/Tiles/Plants/ThunderBorealTree.cs
@@ -0,0 +1,74 @@
+﻿using DevilsWarehouse.Content.Items.Materials;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DevilsWarehouse.Content.Tiles.Plants
+{
+    public class ThunderBorealTree : ModTree
+    {
+
+        private readonly int snow = TileID.SnowBlock;
+        public override TreePaintingSettings TreeShaderSettings => new() 
+        {
+            UseSpecialGroups = true,
+            SpecialGroupMinimalHueValue = 11f / 72f,
+            SpecialGroupMaximumHueValue = 0.25f,
+            SpecialGroupMinimumSaturationValue = 0.88f,
+            SpecialGroupMaximumSaturationValue = 1f
+        };
+
+        public override void SetStaticDefaults()
+        {
+            GrowsOnTileId = new int[1] { snow };
+        }
+
+        public override Asset<Texture2D> GetTexture()
+        {
+            return ModContent.Request<Texture2D>("DevilsWarehouse/Content/Tiles/Plants/ThunderBorealTree");
+        }
+
+        public override int SaplingGrowthType(ref int style)
+        {
+            style = 0;
+            return ModContent.TileType<ThunderBorealSaplingTile>();
+        }
+
+        public override void SetTreeFoliageSettings(Tile tile, ref int xoffset, ref int treeFrame, ref int floorY, ref int topTextureFrameWidth, ref int topTextureFrameHeight)
+        {
+            // This is where fancy code could go, but let's save that for an advanced example
+        }
+
+        // Branch Textures
+        public override Asset<Texture2D> GetBranchTextures()
+        {
+            return ModContent.Request<Texture2D>("DevilsWarehouse/Content/Tiles/Plants/ThunderBorealTree_Branches");
+        }
+
+        // Top Textures
+        public override Asset<Texture2D> GetTopTextures()
+        {
+            return ModContent.Request<Texture2D>("DevilsWarehouse/Content/Tiles/Plants/ThunderBorealTree_Tops");
+        }
+
+        public override int DropWood()
+        {
+            return ModContent.ItemType<AuraliteOre>();
+        }
+
+        public override bool Shake(int x, int y, ref bool createLeaves)
+        {
+            Item.NewItem(WorldGen.GetItemSource_FromTreeShake(x, y), new Vector2(x, y) * 16, snow);
+            return false;
+        }
+
+        public override int TreeLeaf()
+        {
+            return ModContent.GoreType<ThunderBorealLeaf>();
+        }
+    }
+}
