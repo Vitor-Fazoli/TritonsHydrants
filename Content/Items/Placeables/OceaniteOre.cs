0295102501497e7eb0d977ed7e3b97f21d157309
Vitor-Fazoli <vhfazoli@gmail.com>
2025-05-26 14:12:28 -0300

    marking to future content

--- Content/Items/Placeables/OceaniteOre.cs
+++ Content/Items/Placeables/OceaniteOre.cs
@@ -1,25 +1,25 @@
-using Terraria;
-using Terraria.ID;
-using Terraria.ModLoader;
-using TritonsHydrants.Content.Tiles;
+// using Terraria;
+// using Terraria.ID;
+// using Terraria.ModLoader;
+// using TritonsHydrants.Content.Tiles;
 
-namespace TritonsHydrants.Content.Items.Placeables
-{
-    public class OceaniteOre : ModItem
-    {
-        public override void SetStaticDefaults()
-        {
-            Item.ResearchUnlockCount = 100;
-            ItemID.Sets.SortingPriorityMaterials[Item.type] = 58;
-            ItemID.Sets.OreDropsFromSlime[Type] = (3, 13);
-        }
+// namespace TritonsHydrants.Content.Items.Placeables
+// {
+//     public class OceaniteOre : ModItem
+//     {
+//         public override void SetStaticDefaults()
+//         {
+//             Item.ResearchUnlockCount = 100;
+//             ItemID.Sets.SortingPriorityMaterials[Item.type] = 58;
+//             ItemID.Sets.OreDropsFromSlime[Type] = (3, 13);
+//         }
 
-        public override void SetDefaults()
-        {
-            Item.DefaultToPlaceableTile(ModContent.TileType<Oceanite>());
-            Item.width = 12;
-            Item.height = 12;
-            Item.value = 3000;
-        }
-    }
-}
+//         public override void SetDefaults()
+//         {
+//             Item.DefaultToPlaceableTile(ModContent.TileType<Oceanite>());
+//             Item.width = 12;
+//             Item.height = 12;
+//             Item.value = 3000;
+//         }
+//     }
+// }
